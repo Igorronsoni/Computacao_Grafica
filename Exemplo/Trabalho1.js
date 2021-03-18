daad3df9f3274f21115ff3b594caaad4456673ef
Igorronsoni <igorandrey@yahoo.com.br>
2021-03-18 20:18:03 -0300

    Reparação no caminho dos arquivos

--- Exemplo/Trabalho1.js
+++ Exemplo/Trabalho1.js
@@ -2,9 +2,11 @@ var scene;
 var camera;
 var renderer;
 
+var cone;
 var esfera;
 
 var velesfera = [1,0.5];
+var velcone = [-1,-1];
 
 const init = () => {
     scene = new THREE.Scene();
@@ -14,33 +16,67 @@ const init = () => {
     document.body.appendChild(renderer.domElement);
 
     camera.position.z = 500;
-    
+
+    criaCone();
     criaesfera();
 };
 
 const render = () => {
     requestAnimationFrame(render);
 
+    animaCone();
     animaesfera();
 
     renderer.render(scene, camera)
 };
 
 const animaesfera = () => {
-    if (esfera.position.x>=360|| esfera.position.x<=-360) {
-        this.velesfera[0]=velesfera[0]*-1;
+    if (esfera.position.x >= 360 || esfera.position.x <= -360) {
+        this.velesfera[0] = velesfera[0] * -1;
     }
-    if (esfera.position.y>=140 || esfera.position.y<=-140) {
-        this.velesfera[1]=velesfera[1]*-1;
+    if (esfera.position.y >= 150 || esfera.position.y <= -150) {
+        this.velesfera[1] = velesfera[1] * -1;
     }
 
-    this.esfera.position.x+=velesfera[0];
-    this.esfera.position.y+=velesfera[1];
+    this.esfera.position.x += velesfera[0];
+    this.esfera.position.y += velesfera[1];
+}
+
+const animaCone = () => {
+    if (cone.position.x >= 360 || cone.position.x <= -360) {
+        this.velcone[0] = velcone[0] * -1;
+    }
+    if (cone.position.y >= 140 || cone.position.y <= -140) {
+        this.velcone[1] = velcone[1] * -1;
+    }
+
+    this.cone.position.x += velcone[0];
+    this.cone.position.y += velcone[1];
+}
+
+const criaCone = () => {    
+    const radius =  10;  
+    const height = 40;  
+    const radialSegments = 64;  
+    const heightSegments = 64;  
+    const openEnded = true;  
+    const thetaStart = 6;  
+    const thetaLength = 6.3;  
+
+    const geometry = new THREE.ConeGeometry(
+        radius, height,
+        radialSegments, heightSegments,
+        openEnded,
+        thetaStart, thetaLength);
+
+    const material = new THREE.MeshBasicMaterial( {color: "red"} );
+    cone = new THREE.Mesh( geometry, material );
+    scene.add( cone );
 }
 
 const criaesfera = () => {
-    var geometria = new THREE.SphereGeometry(25,25,25);
-    var material = new THREE.MeshBasicMaterial({color:'green'});
+    let geometria = new THREE.SphereGeometry(25,25,25);
+    let material = new THREE.MeshBasicMaterial({color:'green'});
 
     esfera = new THREE.Mesh(geometria, material)
     scene.add(esfera);
